0212f18165e8bdf9b5499f2af0ba438dea0659f5
张亚兵 <noodles@yabing.me>
2016-12-22 11:21:20 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -77,7 +77,7 @@ if (process.env.NODE_ENV === 'production') {
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: '"production"'
+                NODE_ENV: 'production'
             }
         }),
         new webpack.optimize.UglifyJsPlugin({
